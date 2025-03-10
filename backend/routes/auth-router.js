6b767fc0fdc84eb617ca8c123659f63e3ff8c970
Adarsh Mishra <am0070563@gmail.com>
2025-03-10 18:46:59 +0530

    Update auth-router.js

--- backend/routes/auth-router.js
+++ backend/routes/auth-router.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const {loginUser,registerUser, password,get,usercount,like,addPost, start}=require('./aut-controller');
+const {loginUser,registerUser, password,get,usercount,comment,like,addPost, start}=require('./aut-controller');
 const user = require('./user');
 const router=express.Router();
 router.post('/register',registerUser);
@@ -10,7 +10,7 @@ router.get('/get/:email',get);
 router.post('/userwin',usercount);
 router.post('/start',start);
 router.post('/like',like);
-
+router.post('/comment',comment);
 
 
 
